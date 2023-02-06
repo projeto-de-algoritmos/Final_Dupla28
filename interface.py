7899545b6d33fd4a9ca048a215b59dc26ff0dc5a
Lucas Gomes <lggomes007@gmail.com>
2023-02-06 11:46:42 -0300

    adicionando botao de download para arquivo compactado

--- interface.py
+++ interface.py
@@ -10,31 +10,20 @@ st.subheader("O que deseja fazer?")
 opcao = st.radio("", ("Compactar", "Descompactar"))
 
 st.subheader("Selecione um arquivo para compactar")
-uploaded_file = st.file_uploader("Escolha  seu arquivo .txt", type=["txt"])
+uploaded_file = st.file_uploader("Escolha  seu arquivo .txt", type=["txt", "slt28"])
 
 if uploaded_file:
     if opcao == "Compactar":
         # To convert to a string based IO:
         stringio = str(uploaded_file.getvalue().decode("utf-8"))
 
-        # Cria um arquivo nesse diretorio com conteúdo do arquivo original
-        with open(uploaded_file.name, 'w') as f:
-            f.write(stringio)
-
         file = huffman_code.compress(uploaded_file.name)
-        #name = uploaded_file.name.rsplit(".", 1)
-        #name[-1] = "sl28"
-        #new_filename = "_".join(name)
-        # Adicionar uma opção para baixar o arquivo modificado
-        st.markdown("""
-        Para baixar o arquivo modificado, clique no botão abaixo:
 
-        """)
-        st.markdown("""
-        <a href="data:text/plain;charset=utf-8,{}" download="compacted_file.sl28">
-        Baixa arquivo compactado
-        </a>
-        """.format(file), unsafe_allow_html=True)
-        
+        # Botao para baixar o .txt compactado
+        btn = st.download_button(
+                label="Download Arquivo Compactado",
+                data=file,
+                file_name="compacted_file.sl28"
+            )
     else:
         pass
